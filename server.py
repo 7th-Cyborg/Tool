3fe4a2fc68e3a382f96427d9e890bd291f9d6460
Darko Ceranac <7th-Cyborg@users.noreply.github.com>
2022-10-04 11:37:48 +0200

    Update server.py

--- server.py
+++ server.py
@@ -6,11 +6,13 @@ pyautogui.FAILSAFE = False
 sleepSeconds = None
 
 if ((len(sys.argv)<2) or sys.argv[1].isalpha() or int(sys.argv[1])<1):
-    sleepSeconds = 3 * 60
+    sleepSeconds = 2 * 60
 else:
     sleepSeconds = int(sys.argv[1]) * 60
 while(True):
     pyautogui.moveRel(10,0)
+    pyautogui.press("shift")
     time.sleep(0.5)
     pyautogui.moveRel(-10,0)
+    pyautogui.press("shift")
     time.sleep(sleepSeconds)
