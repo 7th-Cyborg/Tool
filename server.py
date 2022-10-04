004e8bf4cb8908b9cf5a4e1ab45b11436693a628
Darko Ceranac <7th-Cyborg@users.noreply.github.com>
2022-10-04 10:38:15 +0200

    PyScript

---
+++ server.py
@@ -0,0 +1,16 @@
+import pyautogui
+import time
+import sys
+
+pyautogui.FAILSAFE = False
+sleepSeconds = None
+
+if ((len(sys.argv)<2) or sys.argv[1].isalpha() or int(sys.argv[1])<1):
+    sleepSeconds = 3 * 60
+else:
+    sleepSeconds = int(sys.argv[1]) * 60
+while(True):
+    pyautogui.moveRel(1,0)
+    time.sleep(0.5)
+    pyautogui.moveRel(-1,0)
+    time.sleep(sleepSeconds)
